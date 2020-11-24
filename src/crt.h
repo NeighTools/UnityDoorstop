3220f45501c13910c7037e32d5fe2dcc72765746
ghorsington <neigh@coder.horse>
2020-11-24 22:41:04 +0200

    Add missing function importer

--- src/crt.h
+++ src/crt.h
@@ -5,6 +5,7 @@
 #include "windows/wincrt.h"
 
 #elif defined(__APPLE__) || defined(__linux__)
+#include <dlfcn.h>
 #include <stdio.h>
 #include <stdlib.h>
 
---
+++ src/func_import.h
@@ -0,0 +1,34 @@
+#include "crt.h"
+
+#define DEFINE_CALLS
+
+#define CAT2(start, middle, end) start##middle##end
+#define CAT(start, middle, end) CAT2(start, middle, end)
+#define STR2(A) #A
+#define STR(A) STR2(A)
+
+#ifndef IMPORT_PREFIX
+#define IMPORT_PREFIX mono
+#endif
+
+#define IMPORT_LIB STR(CAT(IMPORT_PREFIX, .h, ))
+#define LOADER_FUNC_NAME CAT(load_, IMPORT_PREFIX, _funcs)
+
+#define DEF_CALL(retType, name, ...) typedef retType (*name##_t)(__VA_ARGS__);
+#include IMPORT_LIB
+#undef DEF_CALL
+
+struct {
+#define DEF_CALL(retType, name, ...) name##_t name;
+#include IMPORT_LIB
+#undef DEF_CALL
+} IMPORT_PREFIX;
+
+inline void LOADER_FUNC_NAME(void *lib) {
+#define DEF_CALL(retType, name, ...)                                           \
+    IMPORT_PREFIX.name = (name##_t)dlsym(lib, STR(CAT(IMPORT_PREFIX, _, name)));
+#include IMPORT_LIB
+#undef DEF_CALL
+}
+
+#undef DEFINE_CALLS
