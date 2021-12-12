6f2a2946330ef5eb6c01ae2aceffb38bfc9b8bea
ghorsington <neigh@coder.horse>
2021-12-12 23:11:35 +0200

    Revert to original C-based proxy
    The original method based of compilers optimising raw calls into jmps appears to work fine on multiple compilers (tested against MSVC and GCC) while ASM approach requires more considerable work to function properly with multiple compilers.

--- proxygen.lua
+++ proxygen.lua
@@ -1,40 +1,6 @@
 function add_proxydef(filename)
+
     on_load(function(target)
-        local ptr_type = ""
-        if is_arch("x86") then
-            ptr_type = "DWORD"
-        elseif is_arch("x64") then
-            ptr_type = "QWORD"
-        end
-
-        local asm_defs = ""
-        local asm_vars = ""
-        local asm_jmps = ""
-        local proxy_defs = ""
-        local proxy_adds = ""
-        local exports_funcs = ""
-
-        local funcs = io.readfile(filename):split("\n")
-        for i, name in ipairs(funcs) do
-            local func = string.trim(name)
-            asm_defs = asm_defs .. format("public %s\r\n", func)
-            asm_defs = asm_defs .. format("public __%s__\r\n", func)
-
-            asm_vars = asm_vars .. format("  __%s__ %s 0\r\n", func, ptr_type)
-
-            asm_jmps = asm_jmps .. format("%s:\r\n", func)
-            asm_jmps = asm_jmps ..
-                           format("  jmp %s ptr __%s__\r\n", ptr_type, func)
-
-            proxy_defs = proxy_defs ..
-                             format("extern FARPROC __%s__;\r\n", func)
-            proxy_adds = proxy_adds ..
-                             format(
-                                 "__%s__ = GetProcAddress((HMODULE)dll, \"%s\");\r\n",
-                                 func, func)
-
-            exports_funcs = exports_funcs .. func .. "\r\n"
-        end
 
         local tmpl_dir = path.directory(filename)
 
@@ -45,23 +11,43 @@ function add_proxydef(filename)
                            function(w) return tab[w:sub(3, -2)] or w end))
         end
 
-        function write(name)
+        function write(name, replacements)
             local content = io.readfile(path.join(tmpl_dir, name .. ".in"))
-            io.writefile(path.join("build", name), interpolate(content, {
-                ASM_DEFS = asm_defs,
-                ASM_VARS = asm_vars,
-                ASM_JMPS = asm_jmps,
-                PROXY_DEFS = proxy_defs,
-                PROXY_ADDS = proxy_adds,
-                EXPORT_FUNCS = exports_funcs
-            }))
+            io.writefile(path.join("build", name),
+                         interpolate(content, replacements))
         end
 
-        write("dllproxy.asm")
-        write("proxy.c")
-        write("dll.def")
+        local proxy_defs = ""
+        local proxy_adds = ""
+        local proxy_funcs = ""
+        local exports_funcs = ""
+
+        local funcs = io.readfile(filename):split("\n")
+        for i, name in ipairs(funcs) do
+            local func = string.trim(name)
+            proxy_defs = proxy_defs ..
+                             format("static FARPROC __%s__;\r\n", func)
+            proxy_adds = proxy_adds ..
+                             format(
+                                 "__%s__ = GetProcAddress((HMODULE)dll, \"%s\");\r\n",
+                                 func, func)
+            proxy_funcs = proxy_funcs ..
+                              format("void *exp_%s() { return __%s__(); }",
+                                     func, func) .. "\r\n"
+            exports_funcs =
+                exports_funcs .. format("%s = exp_%s", func, func) .. "\r\n"
+        end
+
+        write("proxy.c", {
+            PROXY_DEFS = proxy_defs,
+            PROXY_ADDS = proxy_adds,
+            PROXY_FUNCS = proxy_funcs
+        })
+        write("dll.def", {EXPORT_FUNCS = exports_funcs})
     end)
-    add_files("build/dllproxy.asm")
+
     add_files("build/proxy.c")
-    add_shflags("-def:build/dll.def", {force = true})
+
+    if is_plat("windows") then add_files("build/dll.def") end
+    if is_plat("mingw") then add_shflags("build/dll.def", {force = true}) end
 end
