aaaded9c36887e51576b5d8158e1276586950d54
ghorsington <neigh@coder.horse>
2022-06-14 20:48:16 +0300

    Fix path config not resolving to full path on nix

--- src/nix/config.c
+++ src/nix/config.c
@@ -19,6 +19,15 @@ void try_get_env(const char_t *name, const char_t *def, char_t **target) {
     }
 }
 
+void get_env_path(const char_t *name, char_t **target) {
+    char_t *value = getenv(name);
+    if (value != NULL && strlen(value) > 0) {
+        *target = get_full_path(value);
+    } else {
+        *target = NULL;
+    }
+}
+
 void load_config() {
     get_env_bool("DOORSTOP_ENABLED", &config.enabled);
     get_env_bool("DOORSTOP_REDIRECT_OUTPUT_LOG", &config.redirect_output_log);
@@ -29,11 +38,10 @@ void load_config() {
     get_env_bool("DOORSTOP_MONO_DEBUG_SUSPEND", &config.mono_debug_suspend);
     try_get_env("DOORSTOP_MONO_DEBUG_ADDRESS", TEXT("127.0.0.1:10000"),
                 &config.mono_debug_address);
-    try_get_env("DOORSTOP_TARGET_ASSEMBLY", TEXT("Doorstop.dll"),
-                &config.target_assembly);
-    try_get_env("DOORSTOP_MONO_DLL_SEARCH_PATH_OVERRIDE", NULL,
-                &config.mono_dll_search_path_override);
-    try_get_env("DOORSTOP_CLR_RUNTIME_CORECLR_PATH", NULL,
-                &config.clr_runtime_coreclr_path);
-    try_get_env("DOORSTOP_CLR_CORLIB_DIR", NULL, &config.clr_corlib_dir);
+    get_env_path("DOORSTOP_TARGET_ASSEMBLY", &config.target_assembly);
+    get_env_path("DOORSTOP_MONO_DLL_SEARCH_PATH_OVERRIDE",
+                 &config.mono_dll_search_path_override);
+    get_env_path("DOORSTOP_CLR_RUNTIME_CORECLR_PATH",
+                 &config.clr_runtime_coreclr_path);
+    get_env_path("DOORSTOP_CLR_CORLIB_DIR", &config.clr_corlib_dir);
 }
