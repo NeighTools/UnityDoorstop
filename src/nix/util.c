820e4adb3ca11e7daa883bbb3573ded0cf690c52
ghorsington <neigh@coder.horse>
2022-06-14 23:12:43 +0300

    nix: Add normalize_path

--- src/nix/util.c
+++ src/nix/util.c
@@ -1,5 +1,9 @@
 #include "../util/util.h"
 #include "../crt.h"
+#include <limits.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 char_t *widen(const char *str) {
     char_t *result = (char_t *)malloc(strlen(str) + 1);
@@ -26,9 +30,79 @@ size_t get_module_path(void *module, char_t **result, size_t *size,
     return total_size;
 }
 
+// Adapted from: https://gist.github.com/Eugeny/5127791
+/**
+ * @brief Normalize the path in respect to the current working directory.
+ *
+ * @param pwd Current working directory.
+ * @param src Path to normalize.
+ * @param res Buffer to store the normalized path.
+ * @return char* Pointer to the normalized path (same as res).
+ */
+char *normalize_path(char *pwd, const char *src, char *res) {
+    size_t res_len;
+    size_t src_len = strlen(src);
+
+    const char *ptr = src;
+    const char *end = &src[src_len];
+    const char *next;
+
+    if (src_len == 0 || src[0] != '/') {
+        // relative path
+        size_t pwd_len;
+
+        pwd_len = strlen(pwd);
+        memcpy(res, pwd, pwd_len);
+        res_len = pwd_len;
+    } else {
+        res_len = 0;
+    }
+
+    for (ptr = src; ptr < end; ptr = next + 1) {
+        size_t len;
+        next = (char *)memchr(ptr, '/', end - ptr);
+        if (next == NULL) {
+            next = end;
+        }
+        len = next - ptr;
+        switch (len) {
+        case 2:
+            if (ptr[0] == '.' && ptr[1] == '.') {
+                const char *slash = (char *)memrchr(res, '/', res_len);
+                if (slash != NULL) {
+                    res_len = slash - res;
+                }
+                continue;
+            }
+            break;
+        case 1:
+            if (ptr[0] == '.') {
+                continue;
+            }
+            break;
+        case 0:
+            continue;
+        }
+
+        if (res_len != 1)
+            res[res_len++] = '/';
+
+        memcpy(&res[res_len], ptr, len);
+        res_len += len;
+    }
+
+    if (res_len == 0) {
+        res[res_len++] = '/';
+    }
+    res[res_len] = '\0';
+    return res;
+}
+
 char_t *get_full_path(char_t *path) {
-    char_t *buf = (char_t *)malloc(MAX_PATH);
-    char_t *full_path = realpath(path, buf);
+    char_t *full_path = (char_t *)malloc(MAX_PATH);
+    char_t *cwd_str = getcwd(NULL, MAX_PATH);
+    normalize_path(cwd_str, path, full_path);
+    free(cwd_str);
     if (full_path == NULL) {
         free(full_path);
         return NULL;
