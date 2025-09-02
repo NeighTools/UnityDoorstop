84c075cb5ddf56fa51b35f0287ab544c0e0ced8b
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2025-09-02 09:35:15 +0200

    POSIX-compliant redirect

--- assets/nix/run.sh
+++ assets/nix/run.sh
@@ -92,7 +92,7 @@ for a in "$@"; do
                 rotated=$((rotated+1))
             fi
         done
-        echo 1>&2 "Please set executable_name to a valid name in a text editor"
+        echo "Please set executable_name to a valid name in a text editor" 1>&2
         exit 1
     fi
 done
@@ -104,7 +104,7 @@ if [ -x "$1" ] ; then
 fi
 
 if [ -z "${executable_name}" ] || [ ! -x "${executable_name}" ]; then
-    echo 1>&2 "Please set executable_name to a valid name in a text editor or as the first command line parameter"
+    echo "Please set executable_name to a valid name in a text editor or as the first command line parameter" 1>&2
     exit 1
 fi
 
@@ -151,8 +151,8 @@ case ${os_type} in
     ;;
     *)
         # alright whos running games on freebsd
-        echo 1>&2 "Unknown operating system ($(uname -s))"
-        echo 1>&2 "Make an issue at https://github.com/NeighTools/UnityDoorstop"
+        echo "Unknown operating system ($(uname -s))" 1>&2
+        echo "Make an issue at https://github.com/NeighTools/UnityDoorstop" 1>&2
         exit 1
     ;;
 esac
@@ -190,10 +190,10 @@ case "${file_out}" in
         arch="x86"
     ;;
     *)
-        echo 1>&2 "The executable \"${executable_path}\" is not compiled for x86 or x64 (might be ARM?)"
-        echo 1>&2 "If you think this is a mistake (or would like to encourage support for other architectures)"
-        echo 1>&2 "Please make an issue at https://github.com/NeighTools/UnityDoorstop"
-        echo 1>&2 "Got: ${file_out}"
+        echo "The executable \"${executable_path}\" is not compiled for x86 or x64 (might be ARM?)" 1>&2
+        echo "If you think this is a mistake (or would like to encourage support for other architectures)" 1>&2
+        echo "Please make an issue at https://github.com/NeighTools/UnityDoorstop" 1>&2
+        echo "Got: ${file_out}" 1>&2
         exit 1
     ;;
 esac
